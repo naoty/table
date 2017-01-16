25c788cf7dbaa14436106047f4d17b175558506e
Naoto Kaneko <naoty.k@gmail.com>
2017-01-16 16:44:38 +0900

    Count the length of strings including full width characters

--- row.go
+++ row.go
@@ -1,6 +1,10 @@
 package main
 
-import "strings"
+import (
+	"strings"
+
+	"golang.org/x/text/width"
+)
 
 // Row represents a row in Table.
 type Row []string
@@ -13,10 +17,26 @@ func (row Row) Draw(widths []int) string {
 		buf = append(buf, " ")
 		buf = append(buf, item)
 
-		paddingWidth := widths[i] - len(item)
+		paddingWidth := widths[i] - lenInHalfSize(item)
 		buf = append(buf, strings.Repeat(" ", paddingWidth))
 		buf = append(buf, " |")
 	}
 
 	return strings.Join(buf, "")
+}
+
+func lenInHalfSize(str string) int {
+	len := 0
+
+	for _, r := range str {
+		p := width.LookupRune(r)
+		switch p.Kind() {
+		case width.EastAsianFullwidth, width.EastAsianWide:
+			len += 2
+		default:
+			len++
+		}
+	}
+
+	return len
 }
