85d526886d93194f05c75dfb673dece681551e9b
Naoto Kaneko <naoty.k@gmail.com>
2020-06-28 15:56:19 +0900

    Print help message with -h or --help flag

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
+const HELP_MESSAGE: &str = r#"
+Usage:
+  table -h | --help
+
+Options:
+  -h --help  Print help message
+"#;
+
 fn main() {
-    println!("Hello, world!");
+    for arg in env::args() {
+        match &*arg {
+            "-h" | "--help" => println!("{}", HELP_MESSAGE.trim()),
+            _ => println!("TODO: implement other flags"),
+        }
+    }
 }
