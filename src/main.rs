90ad3eb3cae1e65f07df973109efc3fe11952fe4
Naoto Kaneko <naoty.k@gmail.com>
2020-07-07 21:33:11 +0900

    Add -f option to switch writers

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use clap::{crate_version, App, Arg};
-use csv;
-use io::Write;
+use csv::ReaderBuilder;
 use std::io;
 use std::process;
-use table;
+use table::{ascii_writer, markdown_writer};
 
 const DESCRIPTION: &str = "A command to print ASCII table from stdin";
 const USAGE_TEMPLATE: &str = r#"
@@ -12,6 +11,9 @@ Usage:
 
 Flags:
 {flags}
+
+Options:
+{options}
 "#;
 
 fn main() {
@@ -22,6 +24,15 @@ fn main() {
         .template(USAGE_TEMPLATE.trim())
         .version_short("v")
         .arg(
+            Arg::with_name("format")
+                .short("f")
+                .long("format")
+                .takes_value(true)
+                .default_value("")
+                .value_name("FORMAT")
+                .help("Config input/output data format"),
+        )
+        .arg(
             Arg::with_name("header")
                 .short("H")
                 .long("header")
@@ -29,20 +40,28 @@ fn main() {
         )
         .get_matches();
 
-    if let Err(error) = start(matches.is_present("header")) {
+    if let Err(error) = start(
+        matches.value_of("format").unwrap(),
+        matches.is_present("header"),
+    ) {
         eprintln!("{}", error);
         process::exit(1);
     }
 }
 
-fn start(has_headers: bool) -> io::Result<()> {
-    let mut reader = csv::ReaderBuilder::new()
+fn start(format: &str, has_headers: bool) -> io::Result<()> {
+    let mut reader = ReaderBuilder::new()
         .delimiter(b'\t')
         .has_headers(false)
         .from_reader(io::stdin());
 
-    // let mut writer = ascii_writer::new(io::stdout(), has_headers);
-    let mut writer = table::markdown_writer::new(io::stdout());
+    let tokens = format.split(":").take(2);
+    let mut ascii_writer = ascii_writer::new(io::stdout(), has_headers);
+    let mut markdown_writer = markdown_writer::new(io::stdout());
+    let writer: &mut dyn io::Write = match tokens.last() {
+        Some("markdown") => &mut markdown_writer,
+        _ => &mut ascii_writer,
+    };
 
     for result in reader.records() {
         match result {
