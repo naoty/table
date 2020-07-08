b0d666e6a04ae603af2ae369dfc3c7a1b8ccc85b
Naoto Kaneko <naoty.k@gmail.com>
2020-07-08 21:09:01 +0900

    Add Table type, Read, Write trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
 pub mod ascii_writer;
 pub mod markdown_writer;
+
+pub struct Table {
+  rows: Vec<Vec<String>>,
+  has_headers: bool,
+}
+
+pub struct Error {}
+
+pub trait Read {
+  fn read(&mut self) -> Result<Table, Error>;
+}
+
+pub trait Write {
+  fn write(&self, table: Table) -> Result<(), Error>;
+}
