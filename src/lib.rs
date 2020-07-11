9244a735aff96603c5c4cb818b718c8643c8134c
Naoto Kaneko <naoty.k@gmail.com>
2020-07-11 16:15:04 +0900

    Add column_widths to Table struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use unicode_width::UnicodeWidthStr;
+
 pub mod ascii_writer;
 pub mod markdown_writer;
 pub mod reader;
@@ -25,6 +27,23 @@ impl Table {
   pub fn push_row(&mut self, row: Vec<String>) {
     self.rows.push(row);
   }
+
+  pub fn column_len(&self) -> usize {
+    self.rows.iter().fold(0, |num, row| num.max(row.len()))
+  }
+
+  pub fn column_widths(&self) -> Vec<usize> {
+    let mut max_widths = vec![0; self.column_len()];
+
+    for row in self.rows.iter() {
+      for (i, cell) in row.iter().enumerate() {
+        let max_width = max_widths.get(i).unwrap().clone();
+        max_widths[i] = max_width.max(UnicodeWidthStr::width(cell as &str));
+      }
+    }
+
+    max_widths
+  }
 }
 
 #[derive(Debug)]
@@ -44,4 +63,60 @@ pub trait Read {
 
 pub trait Write {
   fn write(&self, table: Table) -> Result<(), Error>;
+}
+
+mod tests {
+  #[test]
+  fn table_column_len() {
+    let test_cases = vec![
+      (vec![], 0),
+      (vec![vec![]], 0),
+      (vec![vec![String::from("alice")]], 1),
+      (vec![vec![String::from("alice"), String::from("100")]], 2),
+      (
+        vec![
+          vec![String::from("alice"), String::from("100")],
+          vec![String::from("bob")],
+        ],
+        2,
+      ),
+    ];
+
+    for test_case in test_cases {
+      let table = crate::Table::from(test_case.0, false);
+      assert_eq!(table.column_len(), test_case.1);
+    }
+  }
+
+  #[test]
+  fn table_column_widths() {
+    let test_cases = vec![
+      (vec![], vec![]),
+      (vec![vec![]], vec![]),
+      (vec![vec![String::from("alice")]], vec![5]),
+      (
+        vec![vec![String::from("alice"), String::from("80")]],
+        vec![5, 2],
+      ),
+      (
+        vec![
+          vec![String::from("alice"), String::from("80")],
+          vec![String::from("bob"), String::from("100")],
+        ],
+        vec![5, 3],
+      ),
+      (
+        vec![
+          vec![String::from("alice"), String::from("80")],
+          vec![String::from("ボブ"), String::from("100点")],
+        ],
+        vec![5, 5],
+      ),
+    ];
+
+    for test_case in test_cases {
+      let table = crate::Table::from(test_case.0, false);
+      assert_eq!(table.column_widths(), test_case.1);
+    }
+  }
 }
